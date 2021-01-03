bd5cd05bf4cc52d950a8ac00289a2d0b30ee67df
Baryshnikov Nikolay <nabarysh@gmail.com>
2021-01-03 23:00:00 +0300

    hw_done

--- main.py
+++ main.py
@@ -17,25 +17,19 @@ VK_TOKEN = get_token('token.txt')
 
 
 class User:
-    all_users = {}  # индекс всех объектов классов, на всякий случай
+    all_users = {}  # индекс всех объектов классов, чтобы было
 
     def __init__(self):
-        self.all_users.setdefault(type(self).__qualname__, set())
-        self.all_users[type(self).__qualname__].add(self)
+        self.all_users.setdefault(type(self).__qualname__, [])
+        self.all_users[type(self).__qualname__] += [self]
 
 
 class VkUser(User):
-    def __init__(self, some_id):
+    def __init__(self, some_user):
         """инициализаця объекта через обращение к users.get"""
         super().__init__()
-        self.user = requests.get(VK_ENDPOINT + 'users.get', params={'access_token': VK_TOKEN,
-                                                                    'v': VK_VER,
-                                                                    'user_ids': some_id,
-                                                                    'fields': 'counters, '
-                                                                              'domain'}).json()
-        self.user.raise_for_status()
-        self.id = self.user['response'][0]['id']
-        time.sleep(0.5)  # чтобы не забанили, ждём пол секунды
+        self.user = some_user
+        self.id = some_user['response'][0]['id']
 
     def __str__(self):
         """Имя Фамилия: ссылка на профиль"""
@@ -53,10 +47,9 @@ class VkUser(User):
                                                 'source_uid': self.id,
                                                 'target_uid': other.id
                                                    }).json()
-        self.mutual_friends.raise_for_status()
         time.sleep(0.5)
         # каждый объект проходит инициализацию с обращением к users.get поэтому дело не быстрое
-        output = [VkUser(str(entry)) for entry in self.mutual_friends['response']]
+        output = [add_user(str(entry)) for entry in self.mutual_friends['response']]
         return output
 
     def get_self(self):
@@ -69,23 +62,71 @@ def print_users(*args):
 
 
 def get_id_from_url(url: str):
-    return url.split('/')[-1].split('id')[-1]
+    """Разбор ссылки профиля"""
+    result = url.strip().split('/id')[-1].split('/')[-1]
+    if result:
+        output = result
+    else:
+        output = None
+
+    return output
 
 
 def quit_():
-    print('Quit')
+    print('Выход')
     return False
 
 
-def add_user():
-    url = input('введите ссылку на пользователя ВК:\n')
-    temp_id = get_id_from_url(url)
+def add_user(new_id=None):
+    """Создание нового объекта класса пользовалетя ВК.
+    Если ID есть в ВК, возвращает новый экземпляр класса
+    Если такой экземпляр уже есть, то возвращается ссылка на него.
+    Если такого пользователя в ВК нет возвращает True"""
+    if new_id is None:
+        url = input('введите ссылку на пользователя ВК:\n')
+        temp_id = get_id_from_url(url)
+    else:
+        temp_id = new_id
 
-    VkUser(temp_id)
-    return True
+    new_user = requests.get(VK_ENDPOINT + 'users.get', params={'access_token': VK_TOKEN,
+                                                                'v': VK_VER,
+                                                                'user_ids': temp_id,
+                                                               }).json()
+    time.sleep(0.5)  # чтобы не забанили, ждём пол секунды
+    if 'error' in new_user.keys():
+        print(new_user['error']['error_msg'])
+        output = True
+    else:
+        proper_id = new_user['response'][0]['id']
+        is_it_old = check_id(proper_id)
+        if is_it_old:
+            print(f'Такой пользовательь уже есть в моём реестре: {proper_id}')
+            output = is_it_old
+        else:
+            output = VkUser(new_user)
+    return output
+
+
+def check_id(some_id):
+    """"Проверка, есть ли пользователь с таким ID среди уже созданных экземпляров класса
+    Возвращает либо имеющийся экземпляр класса, либо False"""
+    spam = User()
+    spam.all_users.pop(type(spam).__qualname__)
+    if VkUser.__qualname__ in spam.all_users.keys():
+        all_id = [entry.id for entry in spam.all_users[VkUser.__qualname__]]
+        if some_id in all_id:
+            output = spam.all_users[VkUser.__qualname__][all_id.index(some_id)]
+        else:
+            output = False
+
+    else:
+        output = False
+
+    return output
 
 
 def print_all():
+    """Вывод всех имеющихся экземпляров класса"""
     spam = User()
     spam.all_users.pop(type(spam).__qualname__)
     if VkUser.__qualname__ in spam.all_users.keys():
@@ -96,14 +137,35 @@ def print_all():
 
 
 def get_mutual_friends():
-    pass
+    """Функция поиска общих друзей
+    Возвращает список экземпляров - общих друзей, либо True, если друзей нет"""
+    urls = input('введите через запятую ссылки на двух пользователей ВК:\n')
+    id_list = [get_id_from_url(entry) for entry in urls.split(',')]
+    all_trash_entries = [add_user(entry) for entry in id_list]
+    users = [entry for entry in all_trash_entries if isinstance(entry, VkUser)]
+    if len(users) >= 2:
+        result = users[0] & users[1]  # всё как в задании:)
+        if result:
+            output = result
+            print('Общие друзья:')
+            print_users(*output)
+        else:
+            print('У этих пользователей нет общих друзей')
+            output = True
+    else:
+        print('Некорректный ввод пользователей')
+        output = True
 
-
-def test():
-    pass
+    return output
 
 
 def get_action(command_: str):
+    all_commands_ = {'q': quit_,
+                     'a': add_user,
+                     'all': print_all,
+                     'm': get_mutual_friends
+                     }
+
     if command_ in all_commands_.keys():
         output = all_commands_[command_]()
     else:
@@ -121,9 +183,4 @@ def go_vk():
 
 
 if __name__ == '__main__':
-    all_commands_ = {'q': quit_,
-                     'a': add_user,
-                     'all': print_all,
-                     'mut': get_mutual_friends,
-                     'test': test}
     go_vk()
