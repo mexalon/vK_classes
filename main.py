05510ba69cd465d360358c4f7e5dae1ec8fbcb85
Baryshnikov Nikolay <nabarysh@gmail.com>
2021-01-03 20:02:09 +0300

    new add func

--- main.py
+++ main.py
@@ -7,109 +7,123 @@ VK_ENDPOINT = 'https://api.vk.com/method/'
 VK_VER = '5.126'
 
 
-class VkUser:
+def get_token(file_name: str):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        token = f.readline()
+    return token
+
+
+VK_TOKEN = get_token('token.txt')
+
+
+class User:
+    all_users = {}  # индекс всех объектов классов, на всякий случай
+
+    def __init__(self):
+        self.all_users.setdefault(type(self).__qualname__, set())
+        self.all_users[type(self).__qualname__].add(self)
+
+
+class VkUser(User):
     def __init__(self, some_id):
-        self.user = requests.get(VK_ENDPOINT + 'users.get', params={'access_token': get_token(),
+        """инициализаця объекта через обращение к users.get"""
+        super().__init__()
+        self.user = requests.get(VK_ENDPOINT + 'users.get', params={'access_token': VK_TOKEN,
                                                                     'v': VK_VER,
                                                                     'user_ids': some_id,
                                                                     'fields': 'counters, '
                                                                               'domain'}).json()
+        self.user.raise_for_status()
         self.id = self.user['response'][0]['id']
-        time.sleep(0.5)
-
-
+        time.sleep(0.5)  # чтобы не забанили, ждём пол секунды
 
     def __str__(self):
-        output = f"https://vk.com/id{self.id}"
+        """Имя Фамилия: ссылка на профиль"""
+        output = f"{self.user['response'][0]['first_name']}" \
+                 f" {self.user['response'][0]['last_name']}:" \
+                 f" https://vk.com/id{self.id}"
         return output
 
     def __and__(self, other):
+        """Поиск общих друзей через соответствующий метод API"""
         self.mutual_friends = requests.get(VK_ENDPOINT + 'friends.getMutual',
                                            params={
-                                                'access_token': get_token(),
+                                                'access_token': VK_TOKEN,
                                                 'v': VK_VER,
                                                 'source_uid': self.id,
                                                 'target_uid': other.id
                                                    }).json()
-        return self.mutual_friends
+        self.mutual_friends.raise_for_status()
         time.sleep(0.5)
-
-
-
+        # каждый объект проходит инициализацию с обращением к users.get поэтому дело не быстрое
+        output = [VkUser(str(entry)) for entry in self.mutual_friends['response']]
+        return output
 
     def get_self(self):
         pprint(self.user)
 
 
-def get_token():
-    with open('token.txt', 'r', encoding='utf-8') as f:
-        token = f.readline()
-    return token
+def print_users(*args):
+    for entry in args:
+        print(entry)
 
 
+def get_id_from_url(url: str):
+    return url.split('/')[-1].split('id')[-1]
 
-"""стереть тут не забыть!"""
-# class OverflowSearch:
-#     def __init__(self):
-#         pass
-#
-#     def make_search(self, tag: str, hours=1):
-#         """Поиск по тегу за последние hours часов. За день набегает многовато вопросов, решил в часах квантовать"""
-#         endpoint = 'https://api.stackexchange.com/2.2/search'
-#         now = int(time.time())
-#         from_date = str(now - 60 * 60 * hours)
-#         numpage = 1
-#         the_tag = self.check_tag(tag)
-#         if the_tag is None:
-#             print(f'There is no {tag} in tags')
-#             return [{'title': None}]
-#
-#         parameters = {'site': 'stackoverflow',
-#                       'tagged': tag,
-#                       'fromdate': from_date,
-#                       'sort': 'creation',
-#                       'pagesize': '100',
-#                       'page': str(numpage)
-#                       }
-#         has_it_more = True
-#         list_of_things = ['Spam!']
-#         print('Collecting pages:')
-#         while has_it_more:
-#             response = requests.get(endpoint, params=parameters)
-#             response.raise_for_status()
-#             print(numpage)
-#             time.sleep(1)  # чтобы не забанили
-#             list_of_things += response.json()['items']
-#             has_it_more = response.json()['has_more']
-#             numpage += 1
-#             parameters['page'] = str(numpage)
-#
-#         list_of_things.pop(0)
-#         output = tuple(list_of_things)
-#         return output
-#
-#     def check_tag(self, tag: str):
-#         endpoint = f'https://api.stackexchange.com/2.2/tags/{tag}/info'
-#         parameters = {'site': 'stackoverflow'}
-#         response = requests.get(endpoint, params=parameters)
-#         if response.json()['items']:
-#             its_name = response.json()['items'][0]['name']
-#         else:
-#             its_name = None
-#
-#         return its_name
+
+def quit_():
+    print('Quit')
+    return False
+
+
+def add_user():
+    url = input('введите ссылку на пользователя ВК:\n')
+    temp_id = get_id_from_url(url)
+
+    VkUser(temp_id)
+    return True
+
+
+def print_all():
+    spam = User()
+    spam.all_users.pop(type(spam).__qualname__)
+    if VkUser.__qualname__ in spam.all_users.keys():
+        print_users(*spam.all_users[VkUser.__qualname__])
+    else:
+        print('Нет пользователей для вывода')
+    return True
+
+
+def get_mutual_friends():
+    pass
+
+
+def test():
+    pass
+
+
+def get_action(command_: str):
+    if command_ in all_commands_.keys():
+        output = all_commands_[command_]()
+    else:
+        print('неверная команда')
+        output = True
+
+    return output
 
 
 def go_vk():
-    me = VkUser('1258178')
-
-    it1 = VkUser('belindr')
-
-    it2 = VkUser('adres.zanyat')
-
-    pprint(it1 & it2)
-
+    go_go = True
+    while go_go:
+        my_command = input('Введите команду:')
+        go_go = get_action(my_command)
 
 
 if __name__ == '__main__':
+    all_commands_ = {'q': quit_,
+                     'a': add_user,
+                     'all': print_all,
+                     'mut': get_mutual_friends,
+                     'test': test}
     go_vk()
